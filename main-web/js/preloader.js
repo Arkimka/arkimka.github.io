5a5a72ce9b5cfa174a0281830cac49438f35c77c
Arki <arkimka17@gmail.com>
2018-07-03 01:33:38 +0900

    fix-it
    upd-5

--- main-web/js/preloader.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-	setTimeout(function(){
-		var preloader = document.getElementById('page-pre');
-		if(!preloader.classList.contains('done')){
-			preloader.classList.add('done');
-		}
-	},1000);
-}
