b2e2b7f099b27f8fa5d4a2009a55d0adf3c03fc7
Arki <arkimka17@gmail.com>
2019-02-02 16:58:39 +0900

    NewSite
    done1902

---
+++ js/preloader.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+	setTimeout(function(){
+		var preloader = document.getElementById('page-pre');
+		if(!preloader.classList.contains('done')){
+			preloader.classList.add('done');
+		}
+	},1000);
+}
